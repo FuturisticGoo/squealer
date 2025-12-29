074d06e7facb5737c726d7e1230c122b44eb03f2
FuturisticGoo <amananiferfiaff@gmail.com>
2025-12-29 22:31:12 +0530

    Setup version code stuff

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Squealer
 
-Explore SQLite database files
+Explore SQLite database files.   
 
+### Features
+- Show schema of tables and views
+- Show data in the tables and views
+- Execute any queries  
 
+This app is pretty barebones, currently it supports only read-only operations.
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "futuristicgoo.squealer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -30,14 +29,68 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        release {
+            keyAlias keystoreProperties['keyAlias']
+            keyPassword keystoreProperties['keyPassword']
+            storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
+            storePassword keystoreProperties['storePassword']
+        }
+    }
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            applicationIdSuffix ".debug"
+        }
+    }
+
+   flavorDimensions "default"
+   productFlavors {
+       independent {
+            signingConfig signingConfigs.release
+       }
+       fdroid {
+       }
+   }
+
+
+
+    packagingOptions {
+        jniLibs {
+            useLegacyPackaging = true
         }
     }
 }
+
+// See: https://developer.android.com/build/configure-apk-splits
+import com.android.build.OutputFile
+
+// Map for the version code that gives each ABI a value.
+ext.abiCodes = ['armeabi-v7a': 1, 'arm64-v8a': 2, 'x86_64': 3]
+
+android.applicationVariants.all { variant ->
+  // Assigns a different version code for each output APK
+  // other than the universal APK.
+  variant.outputs.each { output ->
+    // Determines the ABI for this variant and returns the mapped value.
+    def baseAbiVersionCode = project.ext.abiCodes.get(output.getFilter(OutputFile.ABI))
+
+    // Because abiCodes.get() returns null for ABIs that are not mapped by ext.abiCodes,
+    // the following code doesn't override the version code for universal APKs.
+    // However, because you want universal APKs to have the lowest version code,
+    // this outcome is desirable.
+    if (baseAbiVersionCode != null) {
+
+      // Assigns the new version code to versionCodeOverride, which changes the
+      // version code for only the output APK, not for the variant itself. Skipping
+      // this step causes Gradle to use the value of variant.versionCode for the APK.
+      output.versionCodeOverride =
+            variant.versionCode * 10 + baseAbiVersionCode
+    }
+  }
+
+
 
 flutter {
     source = "../.."

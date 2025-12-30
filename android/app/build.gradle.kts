1f2510bf3c889694c10113d785d3887a3d66fe3c
FuturisticGoo <amananiferfiaff@gmail.com>
2025-12-30 19:53:19 +0530

    Fix build.gradle signing, flavours and version codes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,32 @@
+import java.util.Properties
+import java.io.FileInputStream
+import com.android.build.gradle.internal.api.ApkVariantOutputImpl
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+var flutterVersionCode = (localProperties["flutter.versionCode"] as String?)?.toIntOrNull()
+if (flutterVersionCode == null) {
+    flutterVersionCode = 1
+}
+var flutterVersionName = localProperties["flutter.versionName"] as String?
+if (flutterVersionName == null) {
+    flutterVersionName = "1.0"
 }
 
 android {
@@ -25,36 +49,36 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = flutterVersionCode
+        versionName = flutterVersionName
     }
 
     signingConfigs {
-        release {
-            keyAlias keystoreProperties['keyAlias']
-            keyPassword keystoreProperties['keyPassword']
-            storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-            storePassword keystoreProperties['storePassword']
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
+    
     buildTypes {
-        release {
-        }
         debug {
-            applicationIdSuffix ".debug"
+            applicationIdSuffix = ".debug"
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
-   flavorDimensions "default"
-   productFlavors {
-       independent {
-            signingConfig signingConfigs.release
-       }
-       fdroid {
-       }
-   }
-
-
+    flavorDimensions += "default"
+    productFlavors {
+        create("independent") {
+            dimension = "default"
+            signingConfig = signingConfigs.getByName("release")
+        }
+        create("fdroid") {
+            dimension = "default"
+        }
+    }
 
     packagingOptions {
         jniLibs {
@@ -63,35 +87,51 @@ android {
     }
 }
 
-// See: https://developer.android.com/build/configure-apk-splits
-import com.android.build.OutputFile
-
-// Map for the version code that gives each ABI a value.
-ext.abiCodes = ['armeabi-v7a': 1, 'arm64-v8a': 2, 'x86_64': 3]
-
-android.applicationVariants.all { variant ->
-  // Assigns a different version code for each output APK
-  // other than the universal APK.
-  variant.outputs.each { output ->
-    // Determines the ABI for this variant and returns the mapped value.
-    def baseAbiVersionCode = project.ext.abiCodes.get(output.getFilter(OutputFile.ABI))
-
-    // Because abiCodes.get() returns null for ABIs that are not mapped by ext.abiCodes,
-    // the following code doesn't override the version code for universal APKs.
-    // However, because you want universal APKs to have the lowest version code,
-    // this outcome is desirable.
-    if (baseAbiVersionCode != null) {
-
-      // Assigns the new version code to versionCodeOverride, which changes the
-      // version code for only the output APK, not for the variant itself. Skipping
-      // this step causes Gradle to use the value of variant.versionCode for the APK.
-      output.versionCodeOverride =
-            variant.versionCode * 10 + baseAbiVersionCode
-    }
-  }
-
-
 
 flutter {
     source = "../.."
 }
+
+
+// Map for the version code that gives each ABI a value.
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
+
+
+// This is the only method that works now
+android.applicationVariants.configureEach {
+    val variant = this
+    variant.outputs.forEach { output ->
+        val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+        if (abiVersionCode != null) {
+            (output as ApkVariantOutputImpl).versionCodeOverride = variant.versionCode * 10 + abiVersionCode
+        }
+    }
+}
+
+// This method is mentioned in the Android documentation but because of a bug
+// with Flutter and Kotlin DSL, the version code is being overriden
+// https://github.com/flutter/flutter/issues/173917
+// androidComponents {
+//     onVariants { variant ->
+
+//         // Assigns a different version code for each output APK
+//         // other than the universal APK.
+//         variant.outputs.forEach { output ->
+//             val name = output.filters.find { it.filterType == ABI }?.identifier
+//             // Stores the value of abiCodes that is associated with the ABI for this variant.
+//             val baseAbiCode = abiCodes[name]
+//             // Because abiCodes.get() returns null for ABIs that are not mapped by ext.abiCodes,
+//             // the following code doesn't override the version code for universal APKs.
+//             // However, because you want universal APKs to have the lowest version code,
+//             // this outcome is desirable.
+
+//             if (baseAbiCode != null) {
+//                 // Assigns the new version code to output.versionCode, which changes the version code
+//                 // for only the output APK, not for the variant itself.
+//                 output.versionCode.set((output.versionCode.get() ?: 0) * 10 + baseAbiCode)
+//             }
+//         }
+//     }
+// }
+
+
